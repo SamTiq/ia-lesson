93c71c8d3bcc9676c920b5eb2eaef5ae00b4f1ec
DEL PUP <samveldelpup@gmail.com>
2022-03-30 08:59:21 +0200

    aled

--- perceptron_with_tools.py
+++ perceptron_with_tools.py
@@ -5,7 +5,7 @@ import torchvision.transforms as T
 
 
 # HYPER-PARAMETERS
-BATCH_SIZE = 64
+BATCH_SIZE = 10
 LEARNING_RATE = 0.0001
 NB_EPOCHS = 100
 
@@ -19,53 +19,41 @@ test_set = torchvision.datasets.FashionMNIST('.', train=False, transform=T.ToTen
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=BATCH_SIZE)
 
+# create the model, define its loss function, and an optimizaiton algorithm
+model = torch.nn.Sequential(torch.nn.Flatten(),
+                            torch.nn.Linear(28*28, 32),
+                            torch.nn.ReLU(),
+                            torch.nn.Linear(32, 10))
 
-# MODEL (Perceptron)
-# Define the weight of the model
-weight = torch.zeros(28*28, 10)
-# Define the bias of the model
-bias = torch.zeros(1, 10)
+criterion = torch.nn.MSELoss()  # fonction de coût
+# optimiseur, ce qui va dire comment les poids se mettent à jour
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-model = torch.nn.Sequential(torch.nn.Flatten(),torch.nn.Linear(28*28, 32), torch.nn.ReLU(), torch.nn.Linear(32,10))
-
-# ITERATIONS
-for epoch in range(NB_EPOCHS):
-    # TRAIN
-    for x, t in train_loader:
-        # PREPROCESS
-        # Flatten the images from 28x28 to 784
-        x = torch.nn.Flatten()(x)
-        # Create one-hot vectors from the targets
-        t = F.one_hot(t, num_classes=10)
-
-        # FORWARD
-        # Compute the output of the model for the input x
-        y = model(x)
-
-        # BACKWARD
-        # Compute the gradient (minus the derivative of the error) on the output layer
-        # Note: Here the error is (t - y)^2
-        grad = 2*(t-y)
-        # Compute delta weight and update the weight 
-        weight += (x.T@grad) * LEARNING_RATE
-        # Compute delta bias and update the bias 
-        bias += grad.sum(0) * LEARNING_RATE
+# lr correspond à la taille des pas
 
 
-    # TEST
+# passe 100 fois sur la database (1 epoch = un parcours de toute la base)
+for epoch in range(100):
+    train_loss = 0.0
+    for x, t in train_loader:  # pour chaque élement de la database
+
+        y = model(x)  # donne l'ordonnée avec l'abcisse si j'ai compris
+
+        # calcule l'erreur avec la prédiction et la target
+        loss = criterion(y, t)
+        loss.backward()  # calcule les deltas de chaque paramètre (w nous donne deltaw)
+        optimizer.step()  # applique chaque delta à son paramètre (w + deltaw)
+        optimizer.zero_grad()  # nettoie le deltaw, le remet à 0
+
+        train_loss += loss/len(train_set)
+
     test_acc = 0.0
+    test_loss = 0.0
+
     for x, t in test_loader:
-        # PREPROCESS
-        # Flatten the images from 28x28 to 784
-        x = torch.nn.Flatten()(x)
-        
-        # FORWARD
-        # Compute the output of the model for the input x
-        y = x@weight + bias
-        
-        # COMPUTE ACCURACY
+        y=model(x)
+
         test_acc += (y.argmax(1) == t).sum()/len(test_set)
-
-
-    # DISPLAY
+        test_loss += criterion(y, t)/len(test_set)
+    
     print('Epoch: {}  |  Acc: {:.3f}%'.format(epoch, test_acc*100))
